cf177a9412c4d21a08a5c11e7913c4a290f84435
13754022243 <shenfengjiao@jingdata.com>
2018-04-15 19:41:21 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import _ from 'lodash'
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
+
 function component() {
     var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    var btn = document.createElement('button');
 
-      // 将图像添加到我们现有的 div。
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  
-  element.appendChild(myIcon);
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
   
     return element;
   }
