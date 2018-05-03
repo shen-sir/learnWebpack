513832318ae117be96239ab530f85ef1dbba672e
13754022243 <shenfengjiao@jingdata.com>
2018-05-03 20:40:51 +0800

    save

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+/* 开发环境配置 */
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  }
+});
