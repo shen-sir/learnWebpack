a44ca6430a408e504a6040e5a4e1d1330d42f723
shen-sir <674735426@qq.com>
2018-05-07 00:45:19 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import _ from 'lodash'
-function component() {
+
+function getComponent() {
+  return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
     var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
     return element;
+
+  }).catch(error => 'An error occurred while loading the component');
   }
-  
-  document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
